704b38c6789cd663e72779c3cebe87b6c10b3470
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-06-30 22:52:13 +0530

    added tests

--- src/index.ts
+++ src/index.ts
@@ -7,34 +7,6 @@ class ApiTracker {
     this.reqCounter = 0;
   }
 
-  async apiTime(fn: () => Promise<any>) {
-    try {
-      this.reqCounter++;
-      const start = process.hrtime();
-      await fn();
-      const end = process.hrtime(start);
-      const dur = end[0] * 1000 + end[1] / 1000000;
-      const time = dur.toFixed(3);
-
-      let performance = "";
-      if (dur < 100) {
-        performance = "🚀 Excellent";
-      } else if (dur < 300) {
-        performance = "✅ Good";
-      } else if (dur < 800) {
-        performance = "⚠️ Acceptable";
-      } else if (dur < 1500) {
-        performance = "🐢 Slow";
-      } else {
-        performance = "🔥 Poor";
-      }
-
-      console.log(`Response Time: ${time}ms - ${performance}`);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   async requestCount() {
     return console.log(this.reqCounter);
   }
@@ -48,4 +20,28 @@ class ApiTracker {
       console.log(error);
     }
   }
+
+  async insights(fn: () => Promise<any>): Promise<void> {
+    try {
+      this.reqCounter++;
+      const start = process.hrtime();
+      await fn();
+      const end = process.hrtime(start);
+      const dur = end[0] * 1000 + end[1] / 1_000_000;
+      const time = dur.toFixed(3);
+
+      let performance = "";
+      if (dur < 100) performance = "🚀 Excellent";
+      else if (dur < 300) performance = "✅ Good";
+      else if (dur < 800) performance = "⚠️ Acceptable";
+      else if (dur < 1500) performance = "🐢 Slow";
+      else performance = "🔥 Poor";
+
+      console.log(`Response Time: ${time}ms - ${performance}`);
+    } catch (error) {
+      console.error("Error in apiTime:", error);
+    }
+  }
 }
+
+export default ApiTracker;
